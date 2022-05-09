08c2f490f5ecbfc157e81523335d9cfa1cb595d9
Alexander Ruchti <ruchtia@dh-node3.hpc.msoe.edu>
2022-05-09 16:00:17 -0400

    word2vec from scratch

--- word2vec/classifier.py
+++ word2vec/classifier.py
@@ -2,11 +2,13 @@ from gensim.models import Word2Vec
 import gensim
 from knn import KNN
 import numpy as np
+from word2vec import Word2VecModel
 
 class Classifier:
     def __init__(self, word2vec_weights):
         self.knn = KNN(5)
-        self.word2vec = Word2Vec.load(word2vec_weights)
+        #self.word2vec = Word2Vec.load(word2vec_weights)
+        self.word2vec = Word2VecModel.load(word2vec_weights)
 
     def _get_average_word_embeddings(self, title):
         word_embeddings = [self.word2vec.wv[x] for x in title.split() if x in self.word2vec.wv]
--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,4 +1,119 @@
-class BaseWord2VecModel():
-    def __init__(self):
-        pass
+from tensorflow.keras import backend
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+import numpy as np
+import tensorflow as tf
+class DataLoader(tf.keras.utils.Sequence):
+
+    """Lazy loading word positive and negative pairs"""
+
+    def __init__(self, word_ids, tokens, batch_size=1024):
+        self.word_ids = word_ids
+        self.tokens = tokens
+        self.batch_size = batch_size
+        self.indices = np.arange(len(tokens))
+
+
+    def __len__(self):
+        return len(self.tokens)//self.batch_size
+
+    def __getitem__(self, batch_index):
+        batch_indices = self.indices[batch_index * self.batch_size:(batch_index + 1) * self.batch_size]
+        selected_tokens = self.tokens[batch_indices]
+        selected_tokens = np.pad(selected_tokens, [(0,0), (0, 1)])
+
+        # Label selections
+        labels = np.ones((self.batch_size, 1), dtype=np.int32)#np.random.randint(2, size=self.batch_size)
+
+        # Ensure we do not take any padding characters
+        first_zeros = (selected_tokens==0).astype(int).argmax(axis=-1).reshape((self.batch_size, 1))
+        source_indices = (np.random.random((self.batch_size, 1)) * first_zeros.reshape((self.batch_size, 1))).astype(int).reshape((self.batch_size, 1))
+        source_final = np.array([selected_tokens[x[0] , x[1] - 1] for x in np.concatenate((np.arange(self.batch_size).reshape((self.batch_size, 1)), source_indices), axis=-1)]).reshape((self.batch_size, 1))
+        targets_final =  np.array([selected_tokens[x[0], x[1]] for x in np.concatenate((np.arange(self.batch_size).reshape((self.batch_size, 1)), source_indices), axis=-1)]).reshape((self.batch_size, 1))
+
+        random_words = np.random.randint(len(self.word_ids) - 1, size=self.batch_size//2) + 1
+        labels[:self.batch_size//2, 0] = 0
+        for i in range(self.batch_size//2):
+            targets_final[i, 0] = random_words[i]
+        return [source_final, targets_final], labels
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.indices)
+
+
+class Word2VecModel():
+    def __init__(self, projection_dim=128):
+        self.word_ids = {}
+        self.projection_dim = projection_dim
+
+    def _pad(self, tokens, length=10):
+        result = []
+        for i in range(len(tokens)):
+            row = ['pad'] * 10
+            for j in range(min(len(tokens[i]), length)):
+                row[j] = tokens[i][j]
+            result.append(row)
+
+        return np.array(result)
+
+    def predict(self, word):
+        if word in self.word_ids:
+            return self.model.predict([self.word_ids[word]])
+        else:
+            return np.zeros(projection_dim)
+
+    def save(self, path):
+        self.model.save(path)
     
+    def load(self, path):
+        self.model = tf.keras.load_model(path)
+
+
+
+    def fit(self, tokens, epochs=32, batch_size=1024):
+        '''
+        @param tokens: A 2d array. Each row contains a set of words from an input description
+        '''
+
+        tokens = self._pad(tokens)
+
+        # Conver the uniue words in the input to a lookup dict
+        unique = np.unique(tokens.flatten())
+        num_unique = len(unique)
+        self.word_ids = {'pad': 0}
+        insert_index = 1
+        for i in range(num_unique):
+             if unique[i] != 'pad':
+                 self.word_ids[unique[i]] = insert_index
+                 insert_index += 1
+        tokens = np.array([self.word_ids[x] for x in tokens.flatten()]).reshape(tokens.shape)
+
+        # Make training model
+        embedding_layer =  layers.Embedding(input_dim=num_unique, output_dim=self.projection_dim)
+
+        # Inputs
+        tokens_1 = layers.Input(shape=(1,))
+        tokens_2 = layers.Input(shape=(1, ))
+        
+        # Get embeddings
+        vec_1 = embedding_layer(tokens_1)
+        vec_2 = embedding_layer(tokens_2)
+
+        # Scale dot products
+        dot = layers.Dot(axes=-1)([vec_1, vec_2])
+        dot = layers.Activation(activation='sigmoid')(dot)
+        training_model = Model([tokens_1, tokens_2], dot)
+
+        # Make model that will be used for actual predictions
+        real_inputs = layers.Input(shape=(1,))
+        out = embedding_layer(real_inputs)
+        self.model = Model(real_inputs, out)
+
+        # Run training
+        data_loader = DataLoader(self.word_ids, tokens, batch_size=batch_size)
+        training_model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+        training_model.fit(x=data_loader, epochs=epochs, verbose=2)
+
+
+
+
